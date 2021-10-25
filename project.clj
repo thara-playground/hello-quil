6abb5a7f0b28b0f26c014543e0ad36f4eaa2f122
thara <git@thara.dev>
2021-10-25 23:18:12 +0900

    :wrench: Fix ClassNotFoundException
    ```
    Execution error (ClassNotFoundException) at jdk.internal.loader.BuiltinClassLoader/loadClass (BuiltinClassLoader.java:641).
    com.apple.eawt.QuitHandler
    ```

--- project.clj
+++ project.clj
@@ -4,4 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [quil "3.1.0"]])
+                 [quil "4.0.0-SNAPSHOT"]])
